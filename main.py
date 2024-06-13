8b841f5c26759f55ed0d26bbaf3fdb7aff2a28df
snilth <suchada.nilth@gmail.com>
2024-06-14 00:06:31 +0700

    install pytube for downloading audio from yt url

--- main.py
+++ main.py
@@ -8,16 +8,25 @@ from kivy.lang import Builder
 # Load .kv file
 Builder.load_file('url_downloader.kv')
 
-# Layout
+# Widget
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+
+# Library
+from pytube import YouTube
 
 class MyApp(App):
     def build(self):
         return URL_Downloader()
 
 class URL_Downloader(BoxLayout):
-    pass
-
+    def download(self):
+        url = self.ids.url_input.text
+        yt = YouTube(url)
+        audio_stream = yt.streams.filter(only_audio=True).first()
+        audio_stream.download(output_path='C:\HobbyProjects\python-kivy-url-downloader\Audio_files')
 
 
 
