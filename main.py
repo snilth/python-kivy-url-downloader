6e0f6b71d1ae5dc6c6d537796388be5b1ef57c08
snilth <suchada.nilth@gmail.com>
2024-06-13 22:33:29 +0700

    Hello, Kivy!

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("URL Downloader by snilth.")
+# URL Downloader by Suchada Nilthuean.
+# Python & Kivy
+
+import kivy
+kivy.require('2.3.0')
+
+from kivy.app import App
+from kivy.uix.label import Label
+
+
+class MyApp(App):
+
+    def build(self):
+        return Label(text='Hello world')
+
+
+if __name__ == '__main__':
+    MyApp().run()
