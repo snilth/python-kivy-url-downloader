0eb7e830170a08357892c01d66d7cddc9ad3b90b
snilth <suchada.nilth@gmail.com>
2024-06-14 21:29:45 +0700

    Added download video func

--- main.py
+++ main.py
@@ -19,7 +19,16 @@ from kivy.uix.button import Button
 from pytube import YouTube
 
 class URL_Downloader(BoxLayout):
-    def download(self):
+    def download_video(self):
+        try:
+            url = self.ids.url_input.text
+            yt = YouTube(url)
+            video_stream = yt.streams.filter(file_extension='mp4')
+            video_stream.get_highest_resolution().download(output_path='C:\HobbyProjects\python-kivy-url-downloader\Video_files')
+        except Exception as e:
+            print(f"Error : {e}")
+
+    def download_audio(self):
         try:
             url = self.ids.url_input.text
             yt = YouTube(url)
