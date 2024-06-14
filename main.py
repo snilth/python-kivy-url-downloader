12797e3c0a26ccff281f0a05d4d983d791bd2bd5
snilth <suchada.nilth@gmail.com>
2024-06-14 21:55:31 +0700

    convert audio file from .mp4 format to .mp3 format after downloaded

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # URL Downloader by Suchada Nilthuean.
 # Python & Kivy
 
+import os
 import kivy
 kivy.require('2.3.0')
 from kivy.app import App
@@ -23,8 +24,12 @@ class URL_Downloader(BoxLayout):
         try:
             url = self.ids.url_input.text
             yt = YouTube(url)
+            video_name = yt.title
             video_stream = yt.streams.filter(file_extension='mp4')
+            print(f"Download : {video_name}")
             video_stream.get_highest_resolution().download(output_path='C:\HobbyProjects\python-kivy-url-downloader\Video_files')
+            print("Done")
+
         except Exception as e:
             print(f"Error : {e}")
 
@@ -32,8 +37,16 @@ class URL_Downloader(BoxLayout):
         try:
             url = self.ids.url_input.text
             yt = YouTube(url)
+            video_name = yt.title
             audio_stream = yt.streams.filter(only_audio=True).first()
-            audio_stream.download(output_path='C:\HobbyProjects\python-kivy-url-downloader\Audio_files')
+            print(f"Download : {video_name}")
+            downloaded_file = audio_stream.download(output_path='C:\HobbyProjects\python-kivy-url-downloader\Audio_files')
+            print("Done")
+
+            base, extension = os.path.splitext(downloaded_file)
+            new_file = base + ".mp3"
+            os.rename(downloaded_file, new_file)
+
         except Exception as e:
             print(f"Error : {e}")
 
